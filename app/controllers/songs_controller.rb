ca12daa710326c1ce3dee3713c29930f82ace2d8
Phoebe Lau <phoebelau7@gmail.com>
2018-01-16 18:09:35 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,4 +27,9 @@ class SongsController < ApplicationController
     erb :'/songs/show'
   end
 
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
 end
