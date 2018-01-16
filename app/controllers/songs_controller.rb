e73d2454d810551d5334868b3062965a899146a2
Phoebe Lau <phoebelau7@gmail.com>
2018-01-16 17:28:44 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,14 +9,13 @@ class SongsController < ApplicationController
   get '/songs/new' do
     erb :'/songs/new'
   end
-  
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
 
   post '/songs/new' do
-    binding.pry
     @song = Song.create(name:params[:name])
     artist = Artist.find_by_name(name:params[:artist][:name])
     if !!artist
