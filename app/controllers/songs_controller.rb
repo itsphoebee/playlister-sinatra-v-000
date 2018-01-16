be9f12b4b7bf289c7ab704e6ad906b700a994504
Phoebe Lau <phoebelau7@gmail.com>
2018-01-16 18:10:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,4 +32,13 @@ class SongsController < ApplicationController
     erb :'/songs/edit'
   end
 
+  post '/songs/:slug' do
+     @song = Song.find_by_slug(params[:slug])
+     @song.update(params[:song])
+     @song.artist = Artist.find_or_create_by(name: params["artist"][:name])
+     @song.save
+     #flash[:message] = "Successfully updated song."
+     redirect "/songs/#{@song.slug}"
+   end
+
 end
